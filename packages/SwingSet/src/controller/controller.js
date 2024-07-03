6792090b6239af65cc69bbf523042b694b3464fc
Turadg Aleahmad <turadg@agoric.com>
2024-07-03 11:32:09 -0700

    refactor: logUnhandledRejections

--- packages/SwingSet/src/controller/controller.js
+++ packages/SwingSet/src/controller/controller.js
@@ -77,14 +77,21 @@ function makeConsole(tagOrTagCreator) {
 }
 
 /**
- * @param {unknown} e
- * @param {Promise} pr
+ * Add a handler for unhandledRejection that logs to the console.
  */
-function unhandledRejectionHandler(e, pr) {
-  // Don't trigger sensitive hosts (like AVA).
-  pr.catch(() => {});
-  console.error('🤞 UnhandledPromiseRejection:', e);
-}
+const logUnhandledRejections = () => {
+  /**
+   * @param {unknown} e
+   * @param {Promise} pr
+   */
+  function loggingHandler(e, pr) {
+    // Don't trigger sensitive hosts (like AVA).
+    pr.catch(() => {});
+    console.error('🤞 UnhandledPromiseRejection:', e);
+  }
+
+  process.on('unhandledRejection', loggingHandler);
+};
 
 /**
  * @param {SwingStoreKernelStorage} kernelStorage
@@ -190,18 +197,7 @@ export async function makeSwingsetController(
   const { kernelBundle = await buildKernelBundle() } = runtimeOptions;
   writeSlogObject({ type: 'bundle-kernel-finish' });
 
-  // FIXME: Put this somewhere better.
-  const handlers = process.listeners('unhandledRejection');
-  let haveUnhandledRejectionHandler = false;
-  for (const handler of handlers) {
-    if (handler === unhandledRejectionHandler) {
-      haveUnhandledRejectionHandler = true;
-      break;
-    }
-  }
-  if (!haveUnhandledRejectionHandler) {
-    process.on('unhandledRejection', unhandledRejectionHandler);
-  }
+  logUnhandledRejections();
 
   function kernelRequire(what) {
     Fail`kernelRequire unprepared to satisfy require(${what})`;
